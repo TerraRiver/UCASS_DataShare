39401385cd7f509c6058902ca49986f4948f0ef1
Unable-teenager <20231081107@ucass.edu.cn>
2025-11-01 15:27:52 +0800

    修复构建bug

--- apps/web-frontend/tailwind.config.ts
+++ apps/web-frontend/tailwind.config.ts
@@ -5,7 +5,9 @@ const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './layouts/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
     '../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
